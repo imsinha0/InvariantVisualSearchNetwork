6f34441bb0fcb24d078d046fb1697499a42d5598
ishaan <ishaan.mohan.sinha@gmail.com>
2025-01-08 18:19:11 -0500

    Array model final

--- utils.py
+++ utils.py
@@ -7,26 +7,42 @@ import numpy as np
 image_positions = {1:(56, 539), 2:(54, 234), 3:(330,85), 4:(592, 230), 5:(584, 530), 6:(337, 675)} #centers
 
 def distance(p1, p2):
-    return np.max(np.abs(p1-p2))
+    #p1 and p2 are tuples
+    return np.max(np.abs(np.array(p1) - np.array(p2)))
 
 def arraySearchProcesswithPath(attentionMap, gtpos):
     numSearches = 0
     searchPath = []
 
+    #convert attentionMap to numpy array
+    attentionMap = attentionMap.numpy()
+
     found = False
     while(not found):
         numSearches += 1
 
+        #print max value in attentionMap, which is a tensor
+        print("max value in attentionMap", np.max(attentionMap))
+
         #get maxpoint as tuple
-        maxPoint = np.unravel_index(attentionMap.argmax(), attentionMap.shape)
+        maxPoint = np.unravel_index(np.argmax(attentionMap), attentionMap.shape) #y then x
+        maxPoint = (maxPoint[1], maxPoint[0]) #x then y
+
         searchPath.append(maxPoint)
+
+        print("searching at", maxPoint)
+
 
         if distance(maxPoint, image_positions[gtpos]) <= 45:
             found = True
         else:
             #set attentionMap to 0 in 45x45 region around maxPoint
             x, y = maxPoint
-            attentionMap[x-22:x+22, y-22:y+22] = 0
+            x_start = max(0, x - 22)
+            x_end = min(attentionMap.shape[1], x + 22)
+            y_start = max(0, y - 22)
+            y_end = min(attentionMap.shape[0], y + 22)
+            attentionMap[y_start:y_end, x_start:x_end] = 0
 
     return numSearches, searchPath
 
